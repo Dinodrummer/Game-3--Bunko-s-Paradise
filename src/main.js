47a6c801bf6a5c004de5df3e321e4e0aed0b5e8a
JT Gruber <jtgruber12@gmail.com>
2026-05-20 12:01:58 -0700

    Bunko's Paradise

---
+++ src/main.js
@@ -0,0 +1,31 @@
+
+// debug with extreme prejudice
+"use strict"
+
+// game config
+let config = {
+    parent: 'phaser-game',
+    type: Phaser.AUTO,
+    render: {
+        pixelArt: true  // prevent pixel art from getting blurred when scaled
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            gravity: {
+                x: 0,
+                y: 1500
+            }
+        }
+    },
+    width: 1440,
+    height: 900,
+    scene: [Load, Platformer]
+}
+
+var cursors;
+const SCALE = 2.0;
+var my = {sprite: {}, text: {}, vfx: {}};
+
+const game = new Phaser.Game(config);
